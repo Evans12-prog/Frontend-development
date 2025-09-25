90b0988519124d363d457643a07fd44835fbcb21
Evans12-prog <evansmenyah7@gmail.com>
2025-09-25 12:16:43 -0800

    main

---
+++ Javascript/operation.js
@@ -0,0 +1,12 @@
+//let num1 = 15;
+//let num2 = 10;
+//const isNumonegreater = num1 <= num2;
+//console.log(isNumonegreater);
+
+//let age = 16;
+//const candrive = 16 ? true : false;
+//console.log(candrive);
+
+let point = 110;
+const overallpoint = point > 100 ? 'gold': 'silver';
+console.log(overallpoint);
---
+++ Javascript/variables.js
@@ -0,0 +1,36 @@
+     //DYNAMIC VARIABLES.
+/*let firstName = 'Evans';
+console.log(typeof firstName);
+firstName = '100';
+console.log(typeof firstName);
+firstName = 'true';
+console.log(typeof firstName);
+*/
+
+let course = {
+   Name:'Javascript for beginners',
+   hour: 3
+};
+//console.log(course.name);
+//course.Name = 'Fundamentals of Javascript';
+//console.log(course['Name']);
+
+//let pete = 'hour';
+//console.log(course[pete]);
+
+                   //ARRAYS
+//let productColor = ['blue','green'];
+//console.log(productColor[0]);
+                      
+                           //FUNCTION
+/*function sayHi(name) {
+   console.log('Hi ' + name);
+}
+
+sayHi('Evans');*/
+
+function multiply(num1,num2) {
+   return num1 * num2;
+}
+
+console.log(multiply(2, 2));
